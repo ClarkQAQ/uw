c621f995c65b490968401a53040001474677cd87
clark <clarkabe@outlook.com>
2024-06-07 12:07:13 +0800

    ✨ feat(ulog): 重构日志轮换写入器

--- ulog/rotate.go
+++ ulog/rotate.go
@@ -1,165 +1,185 @@
 package ulog
 
-// fork from https://github.com/stathat/rotate
-
 import (
+	"compress/gzip"
 	"errors"
 	"fmt"
+	"io"
 	"os"
 	"path"
-	"sort"
-	"strings"
 	"sync"
 	"time"
 )
 
-const (
-	maxDefault  = 1024 * 1024 * 8
-	keepDefault = 10
-)
-
-// RootPerm defines the permissions that Writer will use if it
-// needs to create the root directory.
-var RootPerm = os.FileMode(0o755)
-
-// FilePerm defines the permissions that Writer will use for all
-// the files it creates.
-var FilePerm = os.FileMode(0o666)
-
-// Writer implements the io.Writer interface and writes to the
-// "current" file in the root directory.  When current's size
-// exceeds max, it is renamed and a new file is created.
 type RotateWriter struct {
-	root    string
-	prefix  string
-	current *os.File
-	size    int
-	max     int
-	keep    int
-	sync.Mutex
+	root        string
+	fileFormat  string
+	currentName string
+	current     io.WriteCloser
+	intervalDay int
+	compress    bool
+	rw          sync.RWMutex
+	exited      chan struct{}
 }
 
-// New creates a new Writer.  The files will be created in the
-// root directory.  root will be created if necessary.  The
-// filenames will start with prefix.
-func NewRotateWriter(root string, prefix string) (*RotateWriter, error) {
-	l := &RotateWriter{root: root, prefix: prefix, max: maxDefault, keep: keepDefault}
-	if err := l.setup(); err != nil {
-		return nil, err
+func NewRotateWriter(dir, fileFormat string, intervalDay int, compress ...bool) (*RotateWriter, error) {
+	if intervalDay < 1 {
+		return nil, fmt.Errorf("intervalDay must be >= 1")
 	}
+
+	l := &RotateWriter{
+		root:        dir,
+		fileFormat:  fileFormat,
+		intervalDay: intervalDay,
+		exited:      make(chan struct{}),
+	}
+
+	if len(compress) > 0 {
+		l.compress = compress[0]
+	}
+
+	if e := l.setup(); e != nil {
+		return nil, e
+	}
+
 	return l, nil
 }
 
-// SetMax sets the maximum size for a file in bytes.
-func (r *RotateWriter) SetMax(size int) {
-	r.max = size
-}
+func (r *RotateWriter) Write(p []byte) (int, error) {
+	r.rw.RLock()
+	defer r.rw.RUnlock()
 
-// SetKeep sets the number of archived files to keep.
-func (r *RotateWriter) SetKeep(n int) {
-	r.keep = n
-}
-
-// Write writes p to the current file, then checks to see if
-// rotation is necessary.
-func (r *RotateWriter) Write(p []byte) (n int, err error) {
-	r.Lock()
-	defer r.Unlock()
-	n, err = r.current.Write(p)
-	if err != nil {
-		return n, err
+	if r.current == nil {
+		return 0, errors.New("rotate writer is closed")
 	}
-	r.size += n
-	if r.size >= r.max {
-		if err := r.rotate(); err != nil {
-			return n, err
-		}
-	}
-	return n, nil
+
+	return r.current.Write(p)
 }
 
-// Close closes the current file.  Writer is unusable after this
-// is called.
+func (r *RotateWriter) WriteString(s string) (int, error) {
+	return r.Write([]byte(s))
+}
+
 func (r *RotateWriter) Close() error {
-	r.Lock()
-	defer r.Unlock()
+	r.rw.Lock()
+	defer r.rw.Unlock()
+
 	if err := r.current.Close(); err != nil {
 		return err
 	}
 	r.current = nil
+	r.exited <- struct{}{}
+	defer close(r.exited)
+
 	return nil
 }
 
-// setup creates the root directory if necessary, then opens the
-// current file.
 func (r *RotateWriter) setup() error {
-	fi, err := os.Stat(r.root)
-	if err != nil && os.IsNotExist(err) {
-		err := os.MkdirAll(r.root, RootPerm)
-		if err != nil {
-			return err
+	fi, e := os.Stat(r.root)
+	if e != nil && os.IsNotExist(e) {
+		if e := os.MkdirAll(r.root, os.ModePerm); e != nil {
+			return e
 		}
-	} else if err != nil {
-		return err
+	} else if e != nil {
+		return e
 	} else if !fi.IsDir() {
 		return errors.New("root must be a directory")
 	}
 
-	// root exists, and it is a directory
+	go func(r *RotateWriter) {
+		t := time.NewTimer(r.getNextIntervalDuration())
+		defer t.Stop()
 
-	return r.openCurrent()
-}
+		for {
+			select {
+			case <-r.exited:
+				return
+			case <-t.C:
+				time.Sleep(time.Second)
 
-func (r *RotateWriter) openCurrent() error {
-	cp := path.Join(r.root, "current")
-	var err error
-	r.current, err = os.OpenFile(cp, os.O_RDWR|os.O_CREATE|os.O_APPEND, FilePerm)
-	if err != nil {
-		return err
-	}
-	r.size = 0
-	return nil
-}
+				if e := r.openCurrent(); e != nil {
+					os.Stderr.WriteString("log rotate: " + e.Error() + "\n")
+				}
+			}
 
-func (r *RotateWriter) rotate() error {
-	if err := r.current.Close(); err != nil {
-		return err
-	}
-	filename := fmt.Sprintf("%s_%d", r.prefix, time.Now().UnixNano())
-	if err := os.Rename(path.Join(r.root, "current"), path.Join(r.root, filename)); err != nil {
-		return err
-	}
-	if err := r.clean(); err != nil {
-		return err
-	}
-	return r.openCurrent()
-}
-
-func (r *RotateWriter) clean() error {
-	d, err := os.Open(r.root)
-	if err != nil {
-		return err
-	}
-	names, err := d.Readdirnames(1024)
-	if err != nil {
-		return err
-	}
-	var archNames []string
-	for _, n := range names {
-		if strings.HasPrefix(n, r.prefix+"_") {
-			archNames = append(archNames, n)
+			t.Reset(r.getNextIntervalDuration())
 		}
-	}
-	if len(archNames) <= r.keep {
+	}(r)
+
+	return r.openCurrent()
+}
+
+func (r *RotateWriter) getNextIntervalDuration() time.Duration {
+	nt := time.Now()
+	nt = time.Date(nt.Year(), nt.Month(), nt.Day(), 0, 0, 0, 0, nt.Location())
+	nt = nt.AddDate(0, 0, r.intervalDay)
+
+	return time.Until(nt)
+}
+
+func (r *RotateWriter) openCurrent() (e error) {
+	r.rw.Lock()
+	defer r.rw.Unlock()
+
+	nextName := time.Now().Format(r.fileFormat)
+	if nextName == r.currentName {
 		return nil
 	}
 
-	sort.Strings(archNames)
-	toDel := archNames[0 : len(archNames)-r.keep]
-	for _, n := range toDel {
-		if err := os.Remove(path.Join(r.root, n)); err != nil {
-			return err
+	if r.current != nil {
+		if e := r.current.Close(); e != nil {
+			return e
+		}
+
+		if r.currentName != "" && r.compress {
+			if e := r.compressCurrent(); e != nil {
+				os.Stderr.WriteString("log rotate: " + e.Error() + "\n")
+			} else {
+				os.Remove(path.Join(r.root, r.currentName))
+			}
 		}
 	}
+
+	r.currentName = nextName
+	r.current, e = os.OpenFile(path.Join(r.root, r.currentName),
+		os.O_RDWR|os.O_CREATE|os.O_APPEND|os.O_SYNC, os.ModePerm)
+	return
+}
+
+func (r *RotateWriter) OpenCurrent() error {
+	return r.openCurrent()
+}
+
+func (r *RotateWriter) compressCurrent() error {
+	f, e := os.Open(path.Join(r.root, r.currentName))
+	if e != nil {
+		return e
+	}
+
+	defer f.Close()
+
+	w, e := os.OpenFile(path.Join(r.root, r.currentName+".gz"),
+		os.O_WRONLY|os.O_CREATE|os.O_TRUNC, os.ModePerm)
+	if e != nil {
+		return e
+	}
+	defer w.Close()
+
+	gz, e := gzip.NewWriterLevel(w, gzip.BestCompression)
+	if e != nil {
+		return e
+	}
+	defer gz.Close()
+
+	gz.Comment = fmt.Sprintf("rotate compressed at %s", time.Now().Format(time.RFC3339))
+
+	if _, e := io.Copy(gz, f); e != nil {
+		return e
+	}
+
+	if e := gz.Flush(); e != nil {
+		return e
+	}
+
 	return nil
 }
